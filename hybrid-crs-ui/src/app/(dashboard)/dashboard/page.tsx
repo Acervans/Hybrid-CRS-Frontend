00bb0bde4279164df2dcba33474ed0a7610cfa79
Javier Wang Zhou <60742676+Acervans@users.noreply.github.com>
2025-02-15 00:45:31 +0100

    chore: added floating label input & multi-select input

--- hybrid-crs-ui/src/app/(dashboard)/dashboard/page.tsx
+++ hybrid-crs-ui/src/app/(dashboard)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { getTranslations } from 'next-intl/server'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
 
 export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations('Titles')
@@ -11,6 +12,23 @@ export async function generateMetadata(): Promise<Metadata> {
     title: t('dashboard')
   }
 }
+
+import MultipleSelector, { Option } from '@/components/ui/multiple-selector'
+import { FloatingLabelInput } from '@/components/ui/floating-label-input'
+
+const OPTIONS: Option[] = [
+  { label: 'nextjs', value: 'nextjs' },
+  { label: 'React', value: 'react' },
+  { label: 'Remix', value: 'remix' },
+  { label: 'Vite', value: 'vite' },
+  { label: 'Nuxt', value: 'nuxt' },
+  { label: 'Vue', value: 'vue' },
+  { label: 'Svelte', value: 'svelte' },
+  { label: 'Angular', value: 'angular' },
+  { label: 'Ember', value: 'ember', disable: true },
+  { label: 'Gatsby', value: 'gatsby', disable: true },
+  { label: 'Astro', value: 'astro' }
+]
 
 export default function Page() {
   return (
@@ -28,6 +46,8 @@ export default function Page() {
               <Button variant='outline'>Outline</Button>
               <Button variant='ghost'>Ghost</Button>
               <Button variant='link'>Link</Button>
+              <Input placeholder='Placeholder' />
+              <FloatingLabelInput id={`floating-demo-${x}`} label='Floating Label' labelClassName='bg-card' />
             </CardContent>
             <CardFooter>
               <p>Card Footer</p>
@@ -36,6 +56,13 @@ export default function Page() {
           </Card>
         )
       })}
+      <MultipleSelector
+        defaultOptions={OPTIONS}
+        placeholder='Select frameworks you like...'
+        emptyIndicator={
+          <p className='text-center text-lg leading-10 text-gray-600 dark:text-gray-400'>no results found.</p>
+        }
+      />
     </div>
   )
 }
